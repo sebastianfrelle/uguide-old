e86de941e268c262601efc3642798aea1595d28b
Hmoulvad <Hmoulvad@hotmail.com>
2018-02-26 16:03:13 +0100

    Almost there...

--- uguide/AppDelegate.swift
+++ uguide/AppDelegate.swift
@@ -60,15 +60,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     }
     
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-        if let error = error {
-            // ...
-            return
         }
-        
-        guard let authentication = user.authentication else { return }
-        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
-                                                       accessToken: authentication.accessToken)
-    }
     
     func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
         
--- uguide/authViewController.swift
+++ uguide/authViewController.swift
@@ -14,15 +14,8 @@ class authViewController: UIViewController, GIDSignInDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        GIDSignIn.sharedInstance().uiDelegate = self as! GIDSignInUIDelegate
-        GIDSignIn.sharedInstance().signIn()
         // Do any additional setup after loading the view, typically from a nib.
         }
-    
-    @IBAction func googleLoginBtn(_ sender: GIDSignInButton) {
-        sign(<#T##signIn: GIDSignIn!##GIDSignIn!#>, didSignInFor: <#T##GIDGoogleUser!#>, withError: <#T##Error!#>)
-    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -30,20 +23,12 @@ class authViewController: UIViewController, GIDSignInDelegate {
     }
     
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-        if let error = error {
-            // ...
-            return
-        }
         
         guard let authentication = user.authentication else { return }
         let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
                                                        accessToken: authentication.accessToken)
         
         Auth.auth().signIn(with: credential) { (user, error) in
-            if let error = error {
-                // ...
-                return
-            }
         }
     }
 
