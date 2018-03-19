8df00fa4a6f22e1adfddf59f6e4bd717eabe5e1f
Sebastian Frelle Koch <sebastian.frelle@gmail.com>
2018-03-19 10:57:33 +0100

    Finished table view for courses prototype

--- uguide/Views/Courses/CourseTableViewController.swift
+++ uguide/Views/Courses/CourseTableViewController.swift
@@ -32,27 +32,29 @@ class CourseTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return courses.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cellIdentifier = "CourseTableViewCell"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? CourseTableViewCell else {
+            fatalError("The dequeued cell is not an instance of CourseTableViewCell.")
+        }
+        
+        // Fetches the appropriate meal for the data source layout
+        let course = courses[indexPath.row]
+        
+        cell.courseNameLabel.text = course.name
+        cell.courseIdLabel.text = course.id
+        
         return cell
     }
-    */
-
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
